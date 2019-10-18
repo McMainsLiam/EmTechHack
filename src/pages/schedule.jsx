d1f358aafb41221cfc1bbe31b44cc2278a061e14
mcmainsliam <liam@mcmains.net>
2019-10-17 23:20:38 -0500

    Update schedule.jsx

--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -12,11 +12,13 @@ import config from '../model/config'
 import CustomHeroWide from '../views/CustomHeroWide'
 
 const CustomTitle = styled.p`
-  ${tw`text-grey-light font-extrabold font-sans text-md md:text-xl lg:text-2xl`};
+  padding-top: 20px;
+  ${tw`text-grey-light font-extrabold font-sans text-sm md:text-lg lg:text-xl`};
 `
 
 const CustomSubtitle = styled.p`
-  ${tw`text-grey-light font-semibold font-sans text-sm md:text-lg lg:text-xl`};
+  margin-top: -14px;
+  ${tw`text-grey-light font-sans text-sm md:text-lg lg:text-xl`};
 `
 
 const Schedule = () => (
@@ -29,52 +31,52 @@ const Schedule = () => (
         <Title>Schedule</Title>
 
         <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <div style={{ width: '40%' }}>
-            <CustomTitle>9:00 am</CustomTitle>
+          <div style={{ width: '65%' }}>
+            <CustomTitle>9:00 am - ECSS 2.412</CustomTitle>
             <CustomSubtitle>Hacker Sign In</CustomSubtitle>
 
-            <CustomTitle>10:00 am</CustomTitle>
-            <CustomSubtitle>Walk-In Registration</CustomSubtitle>
-
-            <CustomTitle>10:30 am</CustomTitle>
+            <CustomTitle>10:30 am - ECSS 2.412</CustomTitle>
             <CustomSubtitle>Opening Ceremonies</CustomSubtitle>
 
-            <CustomTitle>11:00 am</CustomTitle>
+            <CustomTitle>11:00 am - ECSW 3.210/3.250</CustomTitle>
             <CustomSubtitle>Begin Hacking</CustomSubtitle>
 
-            <CustomTitle>1:00 pm</CustomTitle>
+            <CustomTitle>1:00 pm - ECSW Lobby</CustomTitle>
             <CustomSubtitle>Lunch</CustomSubtitle>
-          </div>
-          <div style={{ width: '45%' }}>
-            <CustomTitle>1:00 pm</CustomTitle>
-            <CustomSubtitle>Shardus Workshop/Networking</CustomSubtitle>
 
-            <CustomTitle>6:00 pm</CustomTitle>
+            <CustomTitle>1:00 pm - ECSS 2.412</CustomTitle>
+            <CustomSubtitle>Shardus Workshop/Networking</CustomSubtitle>
+          </div>
+          <div style={{ width: '65%' }}>
+            <CustomTitle>6:00 pm - ECSW 2.325</CustomTitle>
             <CustomSubtitle>Intro to Git Workshop</CustomSubtitle>
 
-            <CustomTitle>6:30 pm</CustomTitle>
+            <CustomTitle>6:30 pm - ECSW Lobby</CustomTitle>
             <CustomSubtitle>Dinner</CustomSubtitle>
 
-            <CustomTitle>9:30 pm</CustomTitle>
+            <CustomTitle>9:30 pm - ECSW 2.325</CustomTitle>
             <CustomSubtitle>Intro to Amazon Web Services</CustomSubtitle>
 
-            <CustomTitle>8:00 am</CustomTitle>
+            <CustomTitle>8:00 am - ECSW Lobby</CustomTitle>
             <CustomSubtitle>Breakfast</CustomSubtitle>
-          </div>
-          <div style={{ width: '40%' }}>
-            <CustomTitle>8:30 am</CustomTitle>
+
+            <CustomTitle>8:30 am - ECSW 2.325</CustomTitle>
             <CustomSubtitle>Zabo Workshop</CustomSubtitle>
+          </div>
+          <div style={{ width: '50%' }}>
+            <CustomTitle>10:00 am - ECSW 2.325</CustomTitle>
+            <CustomSubtitle>AI Workshop</CustomSubtitle>
 
             <CustomTitle>11:00 am</CustomTitle>
             <CustomSubtitle>Hacking Ends</CustomSubtitle>
 
-            <CustomTitle>12:00 pm</CustomTitle>
+            <CustomTitle>12:00 pm - ECSW Lobby</CustomTitle>
             <CustomSubtitle>Lunch</CustomSubtitle>
 
-            <CustomTitle>1:00 pm</CustomTitle>
+            <CustomTitle>1:00 pm - ECSW 3.210/3.250</CustomTitle>
             <CustomSubtitle>Judging</CustomSubtitle>
 
-            <CustomTitle>3:00 pm</CustomTitle>
+            <CustomTitle>3:00 pm - ECSW 1.315</CustomTitle>
             <CustomSubtitle>Awards & Prizes</CustomSubtitle>
           </div>
         </div>
